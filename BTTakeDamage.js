dacf74db98feb040ec26f223268d3f90f378e225
Chia-Hao Chang <chaihaoc@gmail.com>
2015-01-25 18:32:51 -0500

    Updates to handle Critical Hits

--- BTTakeDamage.js
+++ BTTakeDamage.js
@@ -5,19 +5,24 @@ var BTTakeDamage = BTTakeDamage || (function() {
         gameInfo.mechName = getAttrByName(gameInfo.characterId, "mech_name");
         //log ("[BTTakeDamage] loc: " + loc + " dam: " + dam + " gameInfo: " + JSON.stringify(gameInfo));
         hitArmor(loc, dam, gameInfo);
+        /*
         var locarmor = loc + "_armor";
         var locAttr = findObjs({
             _characterid: gameInfo.characterId,
             _type: "attribute",
             name: locarmor,
         })[0];
+        */
     }
 
     function hitInternal(loc, dam, gameInfo) {
+        if (loc.length === 3) 
+            loc = loc.substr(0,2);
+
         var locAttr = findObjs({
             _characterid: gameInfo.characterId,
             _type: "attribute",
-            name: this._attributeMap[loc] + "_internalstructure",
+            name: _attributeMap[loc] + "_internalstructure",
         })[0];
         
         var currInternal = locAttr.get("current");
@@ -26,7 +31,7 @@ var BTTakeDamage = BTTakeDamage || (function() {
                 sendChat(gameInfo.who, gameInfo.mechName + " has already been destroyed.");
                 return;
             }
-            var cascadeLoc = this._cascadeMap[loc];
+            var cascadeLoc = _cascadeMap[loc];
             sendChat(gameInfo.who, gameInfo.mechName + " has nothing at " + loc.toUpperCase() + ", damage cascades to " + 
                 cascadeLoc.toUpperCase() + ".");
             return hitArmor(cascadeLoc, dam, gameInfo);
@@ -38,7 +43,7 @@ var BTTakeDamage = BTTakeDamage || (function() {
                 dam + " damage.");
         }        
         else {
-            var cascadeLoc = this._cascadeMap[loc];
+            cascadeLoc = _cascadeMap[loc];
             var newDam = dam - locAttr.get("current", 0);
             locAttr.set("current", 0);
             if (loc === 'ct' || loc === 'hd'){
@@ -56,7 +61,7 @@ var BTTakeDamage = BTTakeDamage || (function() {
         var locAttr = findObjs({
             _characterid: gameInfo.characterId,
             _type: "attribute",
-            name: this._attributeMap[loc] + "_armor",
+            name: _attributeMap[loc] + "_armor",
         })[0];        
 
         if (locAttr === null || typeof locAttr === "undefined") {
@@ -85,6 +90,54 @@ var BTTakeDamage = BTTakeDamage || (function() {
         sendChat(gameInfo.who, gameInfo.mechName + " was hit in the " + loc.toUpperCase() + " for " + dam + " damage.");
     }
     
+    function criticalHitMech(characterId, loc) {
+        loc = loc.toLowerCase();
+        var upperOrLower, hit, component;
+        var baseComp = _attributeMap[loc];
+        var s = 0;
+        
+        var locarmor = loc + "_armor";
+        var locAttr = findObjs({
+            _characterid: characterId,
+            _type: "attribute",
+            name: locarmor,
+        })[0];
+        
+        do {
+            log("[BTTakeDamage] attempt: " + s);
+            upperOrLower = randomInteger(6);
+            hit = randomInteger(6);
+            var componentName = baseComp + "_" + (upperOrLower < 4 ? "a" : "b") + hit.toString();
+            log("BTTakeDamage.criticalHitMech: " + componentName);
+            //criticalhit_lefttorso_b4 = 1 when hit, 0 or undefined when not-hit
+            //lefttorso_b4            
+        } while (component === null || s++ < 10) 
+    }
+    
+    // returns number of critical hits based upon hit location.  -1 if the limb is blown off.
+    function findCriticalHits(loc) {
+        var roll = randomInteger(6) + randomInteger(6);
+        switch (roll) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                return 0;
+            case 8:
+            case 9:
+                return 1;
+            case 10:
+            case 11:
+                return 2;
+            case 12:
+                if (loc === null || loc !== 'lt' || loc !== 'rt' || loc !== 'ct')
+                    return 3;
+                return -1;
+        }
+    }
+    
     // used for mapping the root of the attirbute, doesn't include indicator such as "armor" or "internalstructure"
     _attributeMap = {
         "hd": "head",
@@ -109,8 +162,9 @@ var BTTakeDamage = BTTakeDamage || (function() {
         "rt" : "ct",        
     };
     
-	return {
-		HitMech: hitMech
+    return {
+		HitMech : hitMech,
+        CriticalHitMech : criticalHitMech
 	};
 })();
 
@@ -137,5 +191,9 @@ on("chat:message", function (msg) {
         
         log("[BTTakeDamage] playerid: " + msg.playerid + " characterId: "  + character.id);
         BTTakeDamage.HitMech(loc, dam, { characterId: character.id, who: msg.who });
+    }
+    
+    if (msg.type == "api" && msg.content.indexOf("!ch") !== -1) {        
+        BTTakeDamage.CriticalHitMech("-Jcr6ZjLEX_hLTmW168C", "LL");
     }
 });
